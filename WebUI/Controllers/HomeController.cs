44d6d6cb703eebdf8895d10fbc5871ff734dabe9
abenedykt <arkadiusz@benedykt.net>
2014-04-18 22:13:05 +0200

    empty mvc project

---
+++ WebUI/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
