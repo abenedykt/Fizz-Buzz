212659c71904a722202235782e66627b2227fe20
abenedykt <arkadiusz@benedykt.net>
2014-04-20 20:23:52 +0200

    quick implementation of fizz buzz just to get some real data

--- FizzBuzzApplication/IApplication.cs
+++ FizzBuzzApplication/IApplication.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 
 namespace FizzBuzzApplication
 {
-    public interface IApplication
-    {
-	    IEnumerable<string> Play(int value);
-    }
+	public interface IApplication
+	{
+		IEnumerable<string> Play(int value);
+	}
 
 	public class FizzBuzzApp : IApplication
 	{
 		public IEnumerable<string> Play(int value)
 		{
-			yield return "Hello";
-			yield return "world";
+			for (var i = 1; i <= value; i++)
+			{
+				if (i % 15 == 0) { yield return "Fizz Buzz"; }
+				else if (i % 5 == 0) { yield return "Buzz"; }
+					else if (i % 3 == 0) { yield return "Fizz"; }
+						else yield return i.ToString(CultureInfo.InvariantCulture);
+			}
 		}
 	}
 }
