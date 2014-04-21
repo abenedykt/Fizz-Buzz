a868d66d062d63034d841461ec0aab02c663140a
abenedykt <arkadiusz@benedykt.net>
2014-04-21 20:16:36 +0200

    propper TDD fizz buzz game implementation

---
+++ FizzBuzzApplication/FizzBuzzApp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace FizzBuzzApplication
+{
+	public class FizzBuzzApp : IApplication
+	{
+		public IEnumerable<string> Play(int value)
+		{
+			return Enumerable.Range(1, value).Select(PlayOne);
+		}
+
+		public string PlayOne(int value)
+		{
+			if (value % 15 == 0) 
+				return "Fizz Buzz";
+			if (value % 5 == 0) 
+				return "Buzz";
+			return value%3 == 0 ? "Fizz" : value.ToString(CultureInfo.InvariantCulture);
+		}
+	}
+}
---
+++ Tests/FizzBuzzTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Globalization;
+using FizzBuzzApplication;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace Tests
+{
+    public class FizzBuzzTests
+    {
+	    private readonly FizzBuzzApp _game;
+
+	    public FizzBuzzTests()
+	    {
+		    _game = new FizzBuzzApp();
+	    }
+
+	    [Fact]
+	    public void Test_environment_is_up_and_running()
+	    {
+		    true.Should().BeTrue();
+	    }
+
+	    [Theory]
+		[InlineData(3)]
+		[InlineData(6)]
+		[InlineData(9)]
+		[InlineData(12)]
+	    public void Numbers_that_divide_3_are_returned_as_fizz(int value)
+		{
+			_game.PlayOne(value).Should().Be("Fizz");
+		}
+
+	    [Theory]
+		[InlineData(5)]
+		[InlineData(10)]
+		[InlineData(20)]
+		[InlineData(25)]
+	    public void Numbers_that_divide_5_are_returned_as_buzz(int value)
+		{
+			_game.PlayOne(value).Should().Be("Buzz");
+		}
+
+
+	    [Theory]
+		[InlineData(15)]
+		[InlineData(30)]
+		[InlineData(45)]
+	    public void Numbers_that_divide_3_and_5_are_returned_as_fizz_buzz(int value)
+		{
+			_game.PlayOne(value).Should().Be("Fizz Buzz");
+		}
+
+		[Theory]
+		[InlineData(1)]
+		[InlineData(2)]
+		[InlineData(4)]
+		[InlineData(7)]
+		public void Others_returns_just_a_number(int value)
+		{
+			_game.PlayOne(value).Should().Be(value.ToString(CultureInfo.InvariantCulture));
+		}
+
+	    [Fact]
+	    public void For_range_of_numbers_it_returns_all_consequent_values()
+	    {
+		    _game.Play(5).Should().Equal(new[]
+		    {
+				"1",
+				"2",
+				"Fizz",
+				"4",
+				"Buzz"
+		    });
+	    }
+    }
+}
